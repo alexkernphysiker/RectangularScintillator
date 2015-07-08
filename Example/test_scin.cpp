9eedaad698e8c23a02e4ab836b7f009dd547bfe6
alexkernphysiker <kernphysiker@openmailbox.org>
2015-07-08 13:00:43 +0200

    empty example added

---
+++ Example/test_scin.cpp
@@ -0,0 +1,12 @@
+// this file is distributed under
+// GPL v 3.0 license
+#include <iostream>
+#include <rectscin.h>
+#include <sensitive.h>
+int main(int , char **){
+	printf("Photons number left: %f +/- %f \n",0,0);
+	printf("Photons number right: %f +/- %f \n",0,0);
+	printf("Time resolution left: %f [ns] \n",0);
+	printf("Time resolution right: %f [ns] \n",0);
+	return 0;
+}
