d63c4ff37d5d581dd2d5d8d80f4c006bf2985ffe
alexkernphysiker <kernphysiker@openmailbox.org>
2015-07-10 16:11:28 +0200

    submodule update

--- Example/test_scin.cpp
+++ Example/test_scin.cpp
@@ -7,13 +7,15 @@
 #include <signal_model.h>
 using namespace std;
 int main(int , char **){
+	auto efficiency=[](double l){return 0.3;};
+	auto absorption=[](double l){return 0.08;};
+	double refraction=1.58;
 	RectangularScintillator scintillator(
 		{make_pair(-15,15),make_pair(-7,7),make_pair(-7,7)},
 		TimeDistribution2(0.005,0.2,1.5),
 		RandomValueGenerator<double>([](double l){return 1.0;},360,480,2),
-		1.58,[](double l){return 0.08;}
+		refraction,absorption
 	);
-	auto efficiency=[](double l){return 0.3;};
 	auto leftcnt=make_shared<Counter>();
 	auto rightcnt=make_shared<Counter>();
 	auto lefttime=make_shared<Timer>(0);
