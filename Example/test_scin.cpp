a7ee6319f7dbd9ffcefb488a316d57cd1a75bab4
alexkernphysiker <kernphysiker@openmailbox.org>
2015-07-10 21:05:39 +0200

    Distribution plots

--- Example/test_scin.cpp
+++ Example/test_scin.cpp
@@ -1,6 +1,7 @@
 // this file is distributed under
 // GPL v 3.0 license
 #include <iostream>
+#include <math_h/gnuplot/gnuplot.h>
 #include <rectscin.h>
 #include <lightguide.h>
 #include <sensitive.h>
@@ -20,13 +21,15 @@ int main(int , char **){
 	auto rightcnt=make_shared<Counter>();
 	auto lefttime=make_shared<Timer>(0);
 	auto righttime=make_shared<Timer>(0);
+	auto leftdistr=make_shared<TimeDistribution>(-5,20,200);
+	auto rightdistr=make_shared<TimeDistribution>(-5,20,200);
 	scintillator.Surface(0,RectDimensions::Left)<<(
 		LightGuide({make_pair(-7,7),make_pair(-7,7)},0,1,10)<<(
-			Photosensor({make_pair(-7,7),make_pair(-7,7)},efficiency,0.68)<<leftcnt<<lefttime
+			Photosensor({make_pair(-7,7),make_pair(-7,7)},efficiency,0.68)<<leftcnt<<lefttime<<leftdistr
 		)
 	);
 	scintillator.Surface(0,RectDimensions::Right)<<(
-		Photosensor({make_pair(-7,7),make_pair(-7,7)},efficiency,0.68)<<rightcnt<<righttime
+		Photosensor({make_pair(-7,7),make_pair(-7,7)},efficiency,0.68)<<rightcnt<<righttime<<rightdistr
 	);
 	for(unsigned int cnt=0;cnt<1000;cnt++)
 		scintillator.RegisterGamma({0,0,0},3000);
@@ -34,5 +37,7 @@ int main(int , char **){
 	printf("Photons number right: %f+/-%f\n",rightcnt->average(),rightcnt->sigma());
 	printf("Time left: %f+/-%f\n",lefttime->average(),lefttime->sigma());
 	printf("Time right: %f+/-%f\n",righttime->average(),righttime->sigma());
+	Plotter::Instance().SetOutput(".");
+	Plot<double>().Hist("left",leftdistr->GetDistribution()).Hist("right",rightdistr->GetDistribution());
 	return 0;
 }
