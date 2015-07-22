d303ce5149779aa3640bd0fb68eca38f355850d8
alexkernphysiker <kernphysiker@openmailbox.org>
2015-07-22 17:56:44 +0200

    Mutli signal processing.stage2

--- signal_processing.cpp
+++ signal_processing.cpp
@@ -1,6 +1,7 @@
 // this file is distributed under 
 // GPL v 3.0 license
 #include "math_h/functions.h"
+#include "math_h/interpolate.h"
 #include "signal_processing.h"
 #include "rectscinexception.h"
 using namespace std;
@@ -86,3 +87,50 @@ void ProductWithPowers::Process(Vec&& signals){
 	SendSignalValue(val);
 }
 
+AbstractMultiOutput::AbstractMultiOutput(){}
+AbstractMultiOutput::~AbstractMultiOutput(){}
+AbstractMultiOutput& AbstractMultiOutput::operator>>(shared_ptr< SignalAcceptor > output){
+	m_output_slots.push_back(make_shared<Slot>()>>output);
+	return *this;
+}
+void AbstractMultiOutput::SendEventStart(){
+	for(auto slot:m_output_slots)
+		slot->Start();
+}
+size_t AbstractMultiOutput::GetOutSlotsCount(){
+	return m_output_slots.size();
+}
+void AbstractMultiOutput::SendSignalValue(size_t i, double time){
+	if(i>=m_output_slots.size())
+		throw RectScinException("Mutli output range check error.");
+	m_output_slots[i]->Value(time);
+}
+void AbstractMultiOutput::SendEventEnd(){
+	for(auto slot:m_output_slots)
+		slot->End();
+}
+AbstractMultiOutput::Slot::Slot(){}
+AbstractMultiOutput::Slot::~Slot(){}
+void AbstractMultiOutput::Slot::Start(){SendEventStart();}
+void AbstractMultiOutput::Slot::Value(double time){SendSignalValue(time);}
+void AbstractMultiOutput::Slot::End(){SendEventEnd();}
+
+Single2MultiSignal::Single2MultiSignal(){}
+Single2MultiSignal::~Single2MultiSignal(){}
+void Single2MultiSignal::AcceptEventStart(){SendEventStart();}
+void Single2MultiSignal::AcceptEventEnd(){SendEventEnd();}
+
+Multi2MultiSignal::Multi2MultiSignal(){}
+Multi2MultiSignal::~Multi2MultiSignal(){}
+void Multi2MultiSignal::Start(){SendEventStart();}
+void Multi2MultiSignal::Finish(){SendEventEnd();}
+
+SignalSorter::SignalSorter(){}
+SignalSorter::~SignalSorter(){}
+void SignalSorter::Process(Vec&& signals){
+	Vec out;
+	for(double v:signals)
+		InsertSorted(v,out,std_size(out),std_insert(out,double));
+	for(size_t i=0,n1=out.size(),n2=GetOutSlotsCount();(i<n1)&&(i<n2);i++)
+		SendSignalValue(i,out[i]);
+}
--- signal_processing.h
+++ signal_processing.h
@@ -7,9 +7,9 @@ class SignalPolinomialDistort:public SignalAcceptor,public SignalProducent{
 public:
 	SignalPolinomialDistort(Vec&&coefs);
 	virtual ~SignalPolinomialDistort();
-	virtual void AcceptEventStart()override;
-	virtual void AcceptSignalValue(double time)override;
-	virtual void AcceptEventEnd()override;
+	virtual void AcceptEventStart()final;
+	virtual void AcceptSignalValue(double time)final;
+	virtual void AcceptEventEnd()final;
 private:
 	Vec m_coefs;
 };
@@ -72,6 +72,48 @@ protected:
 private:
 	Vec m_powers;
 };
-class Single2MultiSignal{};
-class Multi2MultiSignal{};
+class AbstractMultiOutput{
+public:
+	AbstractMultiOutput();
+	virtual ~AbstractMultiOutput();
+	AbstractMultiOutput&operator>>(std::shared_ptr<SignalAcceptor>output);
+protected:
+	void SendEventStart();
+	size_t GetOutSlotsCount();
+	void SendSignalValue(size_t i,double time);
+	void SendEventEnd();
+private:
+	class Slot:public SignalProducent{
+	public:
+		Slot();
+		virtual ~Slot();
+		void Start();
+		void Value(double time);
+		void End();
+	};
+	std::vector<std::shared_ptr<Slot>> m_output_slots;
+};
+class Single2MultiSignal:public SignalAcceptor,public AbstractMultiOutput{
+public:
+    Single2MultiSignal();
+    virtual ~Single2MultiSignal();
+	virtual void AcceptEventStart()final;
+	virtual void AcceptSignalValue(double time)=0;
+	virtual void AcceptEventEnd()final;
+};
+class Multi2MultiSignal:public AbstractMultiInput,public AbstractMultiOutput{
+public:
+    Multi2MultiSignal();
+    virtual ~Multi2MultiSignal();
+protected:
+	virtual void Start()final;
+	virtual void Process(Vec&&signals)=0;
+	virtual void Finish()final;
+};
+class SignalSorter:public Multi2MultiSignal{
+    SignalSorter();
+    virtual ~SignalSorter();
+protected:
+    virtual void Process(Vec&& signals)final;
+};
 #endif 
