fbcb44a6776f1a566fa494716b3fb9817741148a
alexkernphysiker <kernphysiker@openmailbox.org>
2015-07-23 17:40:57 +0200

    new tests

---
+++ RectScinTest/statistics.cpp
@@ -0,0 +1,28 @@
+// this file is distributed under 
+// GPL v 3.0 license
+#include <gtest/gtest.h>
+#include <rectscinexception.h>
+#include <signal_statistics.h>
+using namespace std;
+TEST(SignalStatictics,Base){
+	SignalStatictics test;
+	EXPECT_EQ(0,test.data().count());
+	for(size_t i=0;i<10;i++){
+		for(size_t j=1;j<=i;j++){
+			test.AcceptSignalValue(rand());
+			EXPECT_EQ(j,test.data().count());
+		}
+		test.Clear();
+		EXPECT_EQ(0,test.data().count());
+	}
+}
+TEST(SignalDistribution,Base){
+	for(size_t a=0;a<7;a++){
+		double f=rand(),t=f+rand();
+		int cnt=rand()%100;
+		SignalDistribution test(f,t,cnt);
+		EXPECT_EQ(f,test.data().min());
+		EXPECT_EQ(t,test.data().max());
+		EXPECT_EQ(cnt,test.data().size());
+	}
+}
