6449ebf727f69b5cca242284ce46574953df14b3
alexkernphysiker <kernphysiker@openmailbox.org>
2015-07-23 02:22:03 +0200

    signal_statistics upgrade

--- signal_statistics.cpp
+++ signal_statistics.cpp
@@ -1,10 +1,33 @@
 // this file is distributed under 
 // GPL v 3.0 license
 #include "signal_statistics.h"
+#include "rectscinexception.h"
+using namespace std;
 SignalStatictics::SignalStatictics(){}
 SignalStatictics::~SignalStatictics(){}
 void SignalStatictics::AcceptEventStart(){}
-void SignalStatictics::AcceptSignalValue(double time){
-	AddValue(time);
-}
+void SignalStatictics::AcceptSignalValue(double time){AddValue(time);}
 void SignalStatictics::AcceptEventEnd(){}
+
+SignalDistribution::SignalDistribution(string name, double from, double to, int bincount):
+	Distribution<double,double>(from,to,bincount){
+	m_name=name;
+}
+SignalDistribution::~SignalDistribution(){
+	m_plotter.HistWithStdError(m_name,static_cast<Distribution<double>&&>(*this));
+}
+void SignalDistribution::AcceptEventStart(){}
+void SignalDistribution::AcceptSignalValue(double time){AddValue(time);}
+void SignalDistribution::AcceptEventEnd(){}
+
+Signal2DCorrelation::Signal2DCorrelation(string name){m_name=name;}
+Signal2DCorrelation::~Signal2DCorrelation(){
+	m_plotter.WithoutErrors(m_name,static_right(m_data));
+}
+void Signal2DCorrelation::Start(){}
+void Signal2DCorrelation::Process(Vec&& signals){
+	if(signals.size()!=2)
+		throw RectScinException("Signal2DCorrelation: error ");
+	m_data.push_back(make_pair(signals[0],signals[1]));
+}
+void Signal2DCorrelation::Finish(){}
--- signal_statistics.h
+++ signal_statistics.h
@@ -3,13 +3,40 @@
 #ifndef mxhvHUgn
 #define mxhvHUgn
 #include "math_h/sigma.h"
+#include "math_h/interpolate.h"
+#include "math_h/gnuplot/gnuplot.h"
 #include "photon2signal.h"
+#include "signal_processing.h"
 class SignalStatictics:public SignalAcceptor,public Sigma<double>{
 public:
-    SignalStatictics();
-    virtual ~SignalStatictics();
+	SignalStatictics();
+	virtual ~SignalStatictics();
 	virtual void AcceptEventStart()override;
 	virtual void AcceptSignalValue(double time)override;
 	virtual void AcceptEventEnd()override;
 };
+class SignalDistribution:public SignalAcceptor,Distribution<double>{
+public:
+	SignalDistribution(std::string name, double from, double to, int bincount);
+	virtual ~SignalDistribution();
+	virtual void AcceptEventStart()override;
+	virtual void AcceptSignalValue(double time)override;
+	virtual void AcceptEventEnd()override;
+private:
+	Plot<double> m_plotter;
+	std::string m_name;
+};
+class Signal2DCorrelation:public AbstractMultiInput{
+public:
+	Signal2DCorrelation(std::string name);
+	virtual ~Signal2DCorrelation();
+protected:
+	virtual void Start()final;
+	virtual void Process(Vec&&signals)final;
+	virtual void Finish()final;
+private:
+	std::string m_name;
+	std::vector<Pair> m_data;
+	PlotPoints<double,decltype(m_data)> m_plotter;
+}; 
 #endif 
