86682fa0cce86ea2d2c59e29d8ee1fe6e5850ee9
alexkernphysiker <kernphysiker@openmailbox.org>
2015-08-10 15:06:57 +0200

    Isotropic

--- RectScinTest/rectscin.cpp
+++ RectScinTest/rectscin.cpp
@@ -20,3 +20,36 @@ TEST(RectangularScintillator,geometry){
 	EXPECT_NO_THROW(rsc.RegisterGamma({0.5,0.5,0.5},10));
 	EXPECT_THROW(rsc.RegisterGamma({2,2,2},1),RectScinException);
 }
+TEST(RectangularScintillator, Isotropic){
+	RectangularScintillator rsc(
+		{make_pair(0,1),make_pair(0,1),make_pair(0,1)},
+		TimeDistribution1(0.5,1.5),RandomValueGenerator<double>(100,200),
+		1.6,[](double){return 0.0;}
+	);
+	vector<shared_ptr<SignalStatictics>> counts,times;
+	for(size_t dimension=0;dimension<3;dimension++)
+		for(auto side=RectDimensions::Left;side<=RectDimensions::Right;inc(side)){
+			auto ampl=make_shared<SignalStatictics>(),time=make_shared<SignalStatictics>();
+			rsc.Surface(dimension,side)>>(
+				Photosensor({make_pair(0,1),make_pair(0,1)},1.0,[](double){return 1.0;})
+					>>(make_shared<AmplitudeSignal>()>>ampl)
+					>>(TimeSignal({make_pair(0,1)})>>time)
+			);
+			counts.push_back(ampl);
+			times.push_back(time);
+		}
+	for(size_t cnt=0;cnt<200;cnt++)
+		rsc.RegisterGamma({0.5,0.5,0.5},3000);
+	auto check_close=[](vector<shared_ptr<SignalStatictics>>&vec){
+		double val=INFINITY,err;
+		for(shared_ptr<SignalStatictics>one:vec)
+			if(isfinite(val)){
+				EXPECT_TRUE(pow(val-one->data().getAverage(),2)<=pow(err+one->data().getSigma(),2));
+			}else{
+				val=one->data().getAverage();
+				err=one->data().getSigma();
+			}
+	};
+	check_close(counts);
+	check_close(times);
+}
